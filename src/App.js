f47ca8a03af0f3e8bc36caee3413425cb68ae59a
Raph Lee <leer@dickinson.edu>
2021-06-26 11:46:36 -0400

    react router initial comit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FrontPage from "./components/FrontPage/FrontPage";
+import Navbar from "./components/Navbar/Navbar";
+import { Switch, Route, BrowserRouter as Router, withRouter } from "react-router-dom";
+
+
+const Routes = () =>{
+  <Switch>
+    <Route path ="/"  exact component ={FrontPage}/>
+  </Switch>
+}
+
+
 function App() {
-  return <FrontPage />;
+  return (
+    <div className="App">
+      <Router>
+        <Routers/>
+      </Router>
+    </div>
+  )
+  
+  ;
 }
 
 export default App;
